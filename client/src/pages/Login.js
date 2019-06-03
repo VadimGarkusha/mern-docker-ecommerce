950a3cb33f26d8e103443df96954fcdbd0c7661f
Vadym Harkusha <vadgig@gmail.com>
2019-06-03 02:17:11 -0400

    Implemeted social login wiht facebook and google on front end

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,33 +1,59 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import AuthenticationUIWrapper from '../components/AuthenticationUIWrapper';
-
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF as faCoffee } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import AuthenticationUIWrapper from "../components/AuthenticationUIWrapper";
 
 const styles = theme => ({
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%",
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    margin: theme.spacing(3, 0, 1)
+  },
+  socialLoginButton: {
+    width: '100%',
+    margin: '5px 0',
+    borderRadius: 2
+  },
+  facebookButton: {
+    background: '#4D69B9',
+    height: 43,
+    boxShadow: 'rgba(0, 0, 0, 0.24) 0px 2px 2px 0px, rgba(0, 0, 0, 0.24) 0px 0px 1px 0px',
+    borderColor: '#d8d8d8',
+    color: '#fff',
+    border: 'transparent',
+    paddingLeft: 11,
+    textAlign: 'left',
+    fontSize: 14,
+    fontFamily: 'Helvetica, Arial, "lucida grande",tahoma,verdana,arial,sans-serif'
   }
 });
 
-
 class Login extends React.Component {
+  responseFacebook = (response) => {
+    console.log(response);
+  }
 
-  renderForm = (classes) => (
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  renderForm = classes => (
     <div>
-      <Typography component="h1" variant="h5" align='center'>
+      <Typography component="h1" variant="h5" align="center">
         Sign in
-        </Typography>
+      </Typography>
       <form className={classes.form} noValidate>
         <TextField
           variant="outlined"
@@ -63,12 +89,30 @@ class Login extends React.Component {
           className={classes.submit}
         >
           Sign In
-          </Button>
+        </Button>
+        <div style={{flexWrap: 'wrap' }} >   
+          <FacebookLogin
+            cssClass={`${classes.socialLoginButton} ${classes.facebookButton}`}
+            icon={<FontAwesomeIcon icon={faCoffee} size='2x' style={{ marginRight: 25, fontSize: '1.5em'}}/>}
+            appId="292213141686047"
+            fields="name,email,picture"
+            callback={this.responseFacebook}
+            textButton='Continue with Facebook'
+          />
+        <GoogleLogin
+          className={classes.socialLoginButton}
+          clientId="586788897755-ueejkhs6hfo0vl4nmku73h3lte9k3f71.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+        </div>
+
         <Grid container>
           <Grid item xs>
             <Link href="#" variant="body2">
               Forgot password?
-              </Link>
+            </Link>
           </Grid>
           <Grid item>
             <Link href="/signup" variant="body2">
