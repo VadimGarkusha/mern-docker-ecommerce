cf8ffecd13c9cfb6f2ba1b529e3e454d83fa4494
Vadym Harkusha <vadgig@gmail.com>
2019-06-15 23:39:10 -0400

    Added User model

---
+++ api/models/UserModel.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const { Schema } = mongoose;
+
+const UserModel = new Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    },
+    isSocial: {
+      type: Boolean,
+      default: false
+    },
+    socialId: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+// hash the password
+UsersModel.methods.generateHash = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+// checking if password is valid
+UsersModel.methods.validPassword = (password) =>
+  bcrypt.compareSync(password, this.password);
+
+module.exports = mongoose.model('Users', UserModel);
